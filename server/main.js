7d5305e39914dd950f70dd722cf81245137ef8a5
Alexander Wieser <alexander.wieser@crystalbyte.de>
2024-01-04 01:28:47 +0100

    Changed server cache

--- server/main.js
+++ server/main.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 const PORT = env.process.PORT || 3000;
 
-app.use(express.static("webgl-urp-2d-sample/dist"));
+const app = express();
 app.enableCors();
+
+const routes = ["webgl-urp-2d-sample"];
+for (const route of routes) {
+  app.use(
+    route,
+    express.static(path.join(__dirname, route), {
+      cacheControl: "no-cache",
+    })
+  );
+}
+
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
